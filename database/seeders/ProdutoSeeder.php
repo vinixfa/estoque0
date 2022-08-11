64f94035811a6f59ceaed44c1d85e6151778c957
Vinícius Almeida <91106730+vinixfa@users.noreply.github.com>
2022-08-10 21:52:45 -0300

    atualização

--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\model\produtos;
+use app\models\Produto;
 
 class ProdutoSeeder extends Seeder
 {
