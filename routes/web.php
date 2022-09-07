b5d75957d7a788b9189f0c3b5f3a9725079468cc
vinixfa <91106730+vinixfa@users.noreply.github.com>
2022-09-07 16:49:15 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use App\Http\Controllers\ProdutoController;
 | contains the "web" middleware group. Now create something great!
 |
 */ 
+
+// CRUD
+
 Route::get('/', function () {
     return view('welcome');
 });
